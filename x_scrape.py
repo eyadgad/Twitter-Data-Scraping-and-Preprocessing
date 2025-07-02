18f486c9feb4f1e30e77892af3342be1b2ce6b70
Eyad Gad <eyademg@yahoo.com>
2025-07-02 10:04:26 -0400

    Adding scraping part

--- x_scrape.py
+++ x_scrape.py
@@ -56,3 +56,95 @@ for from_account in from_accounts:
     end = datetime.strptime(until_date, "%Y-%m-%d")
     since_step = since_date
     k=0
+
+    
+    for i, until_step in enumerate(date_range(start, end, 15)):
+        if until_step == since_step:continue
+
+        shutil.rmtree(cookies_directory, ignore_errors=True)
+        os.makedirs(cookies_directory, exist_ok=True)
+
+        save_directory = f'{from_account}/{since_step}_{until_step}'
+        if k >= len(envfiles): k=0
+        env_path = f'environments/{k}.env'
+        
+        scweet = Scweet(
+            proxy=proxy,
+            cookies=cookies,
+            cookies_path=cookies_directory,
+            user_agent=user_agent,
+            disable_images=disable_images,
+            env_path=env_path,
+            n_splits=calc_n_splits(since_step, until_step, days_interval) ,
+            concurrency=concurrency,
+            headless=headless,
+            scroll_ratio=scroll_ratio
+        )
+
+        tweets = scweet.scrape(
+            since = since_step,
+            until = until_step,
+            words = query_words,
+            to_account = to_account,
+            from_account = from_account,
+            mention_account = mention_account,
+            lang = language,
+            limit = limit_tweets,
+            display_type = display_type,
+            resume = resume_scraping,
+            hashtag = hashtag,
+            filter_replies = filter_replies,
+            proximity = proximity_search,
+            geocode = geocode_location,
+            minreplies = min_replies,
+            minlikes = min_likes,
+            minretweets = min_retweets,
+            save_dir = save_directory,
+            custom_csv_name = custom_csv_filename
+        )     
+
+        json_files = [os.path.join(save_directory, f) for f in os.listdir(save_directory) if "data" in f and f.endswith('.json')]
+        data_dict = {"id":[],"name":[],"username": [], "time": [], "text": [], "counts": []}
+        for json_file in json_files:
+            with open(json_file, 'r') as file: json_data = json.load(file)
+            data_dict = read_tweets(json_data, data_dict, csv=True, show_text=False)
+            os.remove(json_file)
+
+        save_dict_to_csv(data_dict, os.path.join(save_directory, "tweets_from_json.csv"))
+        
+        since_step = until_step
+        k+=1
+        time.sleep(20)
+
+    l = [os.path.join(from_account, i) for i in os.listdir(from_account) ]
+    tweets_files = [os.path.join(i, custom_csv_filename) for i in l if os.path.exists(os.path.join(i, custom_csv_filename))]
+
+    # replace with your actual file list
+    final_csv_file = f"{from_account}_tweets.csv"
+
+    # Flag to write header only once
+    merge_csv_files(tweets_files, final_csv_file)
+
+    df = pd.read_csv(final_csv_file, dtype={"tweetId": str})
+    df['url_id'] = df['Tweet URL'].apply(extract_id_from_url)
+
+    # Replace tweet_id with url_id if they don't match and url_id is valid
+    df['tweetId'] = df.apply(
+        lambda row: row['url_id'] if row['url_id'] and row['tweetId'] != row['url_id'] else row['tweetId'],
+        axis=1
+    )
+    # replace \n with space in Text column
+    df['Text'] = df['Text'].str.replace('\n', ' ', regex=False)
+    df['Text'] = df['Text'].str.replace('\r', ' ', regex=False)
+    df['Text'] = df['Text'].str.replace('http', ' http', regex=False)
+
+
+    # Drop the helper column
+    df.drop(columns=['url_id'], inplace=True)
+    df.drop(columns=['Embedded_text'], inplace=True, errors='ignore')
+    df.drop(columns=['Emojis'], inplace=True, errors='ignore')
+    df.drop(columns=['Image link'], inplace=True, errors='ignore')
+    df.drop_duplicates(subset='tweetId', inplace=True)
+    df.sort_values(by='Timestamp', inplace=True)
+
+    df.to_csv(final_csv_file, index=False)
